19550d8a5a27c1ccb44c633b3b5b0a2178f620c3
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-03-27 07:53:30 -0600

    Add passing tests for /api/v1/books endpoint

---
+++ spec/requests/api/v1/books_request_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "Books API" do
+  it "sends a list of books" do
+    create_list(:book, 3)
+
+    get '/api/v1/books'
+    #this is the REQUEST we are making to the server
+    # binding.pry
+    expect(response).to be_successful
+
+    books = JSON.parse(response.body, symbolize_names: true)
+
+    expect(books.count).to eq(3)
+    books.each do |book|
+      expect(book).to have_key(:id)
+      expect(book[:id]).to be_an(Integer)
+
+      expect(book).to have_key(:title)
+      expect(book[:title]).to be_a(String)
+
+      expect(book).to have_key(:author)
+      expect(book[:author]).to be_a(String)
+
+      expect(book).to have_key(:genre)
+      expect(book[:genre]).to be_a(String)
+
+      expect(book).to have_key(:summary)
+      expect(book[:summary]).to be_a(String)
+
+      expect(book).to have_key(:number_sold)
+      expect(book[:number_sold]).to be_an(Integer)
+    end
+  end
+end
