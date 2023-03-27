c8e70e731e2294f9a39ff204f313d5c05623599a
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-03-27 08:27:24 -0600

    Add BooksController#Update

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :books, only: [:index, :show, :create]
+      resources :books, only: [:index, :show, :create, :update]
     end
   end
 end
--- spec/requests/api/v1/books_request_spec.rb
+++ spec/requests/api/v1/books_request_spec.rb
@@ -70,7 +70,7 @@ RSpec.describe "Books API" do
       summary: 'Filled with suspense.',
       number_sold: 432
     })
-    
+
     headers = {"CONTENT_TYPE" => "application/json"}
 
     # We include this header to make sure that these params are passed as JSON rather than as plain text
@@ -83,5 +83,20 @@ RSpec.describe "Books API" do
     expect(created_book.summary).to eq(book_params[:summary])
     expect(created_book.genre).to eq(book_params[:genre])
     expect(created_book.number_sold).to eq(book_params[:number_sold])
+  end
+
+  it "can update an existing book" do
+    id = create(:book).id
+    previous_name = Book.last.title
+    book_params = { title: "Charlotte's Web" }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    # We include this header to make sure that these params are passed as JSON rather than as plain text
+    patch "/api/v1/books/#{id}", headers: headers, params: JSON.generate({book: book_params})
+    book = Book.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(book.title).to_not eq(previous_name)
+    expect(book.title).to eq("Charlotte's Web")
   end
 end
